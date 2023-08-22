09a84bc54258e50186bed2a4cfb281eb3bc8005b
Lauro <laurolamas@gmail.com>
2023-08-22 13:11:19 -0300

    Added_1

---
+++ api/1-export_to_CSV.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+""" Task 1 """
+
+
+def write_file(filename="", text=""):
+    """ Write file """
+    with open(filename, mode="a", encoding="utf-8") as f:
+        return f.write(text)
+
+
+if __name__ == '__main__':
+    import requests
+    import sys
+
+    if len(sys.argv) != 2:
+        print("Enter a valid number")
+        sys.exit()
+
+    UserId = sys.argv[1]
+
+    userTasks = requests.get(
+        f'https://jsonplaceholder.typicode.com/users/{UserId}/todos').json()
+    userInfo = requests.get(
+        f'https://jsonplaceholder.typicode.com/users/{UserId}').json()
+
+    userName = userInfo.get('name')
+    text = ""
+    for task in userTasks:
+        text += f"\"{UserId}\",\"{userName}\",\""
+        f"{task.get('completed')}\",\"{task.get('title')}\"\n"
+
+    text = text[:-1]
+    write_file(f"{UserId}.csv", text)
