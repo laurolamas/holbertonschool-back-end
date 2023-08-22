1a096b34a018dce4c3783fa4420b1e396cbb165c
Lauro <laurolamas@gmail.com>
2023-08-22 12:50:58 -0300

    Added_0

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" Task 0 """
+
+
+if __name__ == '__main__':
+    import requests
+    import sys
+
+    if len(sys.argv) != 2:
+        print("Enter a valid number")
+        sys.exit()
+
+    taskId = sys.argv[1]
+
+    userTasks = requests.get(f'https://jsonplaceholder.typicode.com/users/{taskId}/todos').json()
+    userInfo = requests.get(f'https://jsonplaceholder.typicode.com/users/{taskId}').json()
+
+    userName = userInfo.get('name')
+    completedTasks = [todo for todo in userTasks if todo['completed']]
+    print(f"Employee {userName} is done with tasks({len(completedTasks)}/{len(userTasks)}):")
+    for task in completedTasks:
+        print(f"\t {task.get('title')}")
